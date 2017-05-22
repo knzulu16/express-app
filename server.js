f87259ff451e13f3ef94bcb393304307e407e015
Nonzulu <knzulu16@gmail.com>
2017-05-22 11:24:24 +0200

    save counter route

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 var express = require('express');
 // const SubjectRoutes=require('/subjects');
 // const subjectRoutes=SubjectsRoutes();
+var nameCounts={};
 var app = express();
-
-var count = 0;
+var namesGreeted='';
 var greeted=[];
+
 
 app.get('/', function(req, res){
     res.send("Hello, Kala!")
 });
 
-app.get('/Greetings/:id', function(req, res){
-  greeted.push({'name':req.params.id});
-  res.send("Greetings: " + req.params.id)
+app.get('/Greetings/:names', function(req, res){
+  greeted.push({'name':req.params.names});
+  res.send("Greetings: " + req.params.names);
 });
 
 app.get('/Greeted', function(req, res){
   res.send(greeted);
 });
 
-app.get('/Counter', function(req, res){
-  count++;
-  res.send("Greeted"+count+"times");
+app.get('/Counter/:name', function(req, res){
+var name = req.params.name;
+var greetingsCount = 0;
+
+for(var i=0;i<greeted.length;i++){
+  if(greeted[i].name === name){
+    greetingsCount++;
+}
+  // greeted.forEach(function(nam
+  }
+    // console.log(nameCounts);
+    res.send("Has been greeted"+""+greetingsCount+""+"times");
+    // res.send(greeted.length);
 });
-
-
 
 const port=8000;
 app.listen(port, function(){
@@ -41,4 +50,3 @@ console.log('web app started on port:'+port);
 //
 //  console.log('Example app listening at http://%s:%s', host, port);
 //
-// });
