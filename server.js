27d5d3c0169bc58fdcc808496e397da377aca85f
Nonzulu <knzulu16@gmail.com>
2017-08-16 15:14:18 +0200

    adding some changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/Greetings";
-mongoose.connect(mongoURL);
+// mongoose.connect(mongoURL);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -166,45 +166,9 @@ app.get('/Greeted', function(req, res) {
 //   });
 
 
-// app.post('/Greeted',function(req,res){
-//   var name=req.body.view;
-//   var uniqueNames=[];
-//   for(var i=0;i<greeted.length;i++){
-//     if(uniqueNames.indexOf(greeted[i])===-1){
-//       uniqueNames.push(greeted[i]);
-//     }
-//     console.log(uniqueNames);
-//     res.render('index.handlebars',{
-//       output:uniqueNames
-//     });
-//   }
-//
-// });
 
-// app.post('/Greetings', function(req, res) {
-//   res.render('index', {
-//     output: msg
-//   });
-// });
-// app.get('/', function(req, res){
-//     res.send("Hello, Kala!")
-// });
-
-
-
-// app.get('/Greeted', function(req, res) {
-//   var name = req.params.name;
-//   var uniqueNames = [];
-//
-//   for (var i = 0; i < greeted.length; i++) {
-//     if (uniqueNames.indexOf(greeted[i]) === -1) {
-//       uniqueNames.push(greeted[i]);
-//       //  Greetings[greetedNames]=0;
-//     }
-//   }
-//   res.send(uniqueNames);
-// });
 app.get('/Counter', function(req, res) {
+
   res.render('index.handlebars', {
     names: greeted
   });
@@ -212,9 +176,18 @@ app.get('/Counter', function(req, res) {
 
 
 app.post('/Counter/:names', function(req, res) {
+  var name = req.params.names;
+  var greetingsCount = 0;
+  for(var i=0;i<greeted.length;i++){
+    if(greeted[i] === name){
+      greetingsCount++;
+  }
 
+    }
+    res.send("Has been greeted" +' '+ greetingsCount +' ' +"time(s)");
 
-});
+  });
+
 app.use(function(err, req, res, next) {
   console.error(err.stack)
   res.status(500).send(err.stack)
